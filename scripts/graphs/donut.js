56ce163421ace2bec07eef3062febe3ab67fc127
michal <michal.barnisin14@gmail.com>
2023-01-10 03:11:36 +0100

    [feat] Add example donut graph.

---
+++ scripts/graphs/donut.js
@@ -0,0 +1,121 @@
+/** donut chart, from https://d3-graph-gallery.com/graph/donut_label.html **/
+
+const donutData = [
+    { a: 9 },
+    { a: 8 },
+    { a: 5 },
+    { a: 8 },
+    { a: 8 },
+    { a: 8 },
+    { a: 9 },
+    { a: 9 },
+];
+const donutAttr = "a";
+
+/**
+ *
+ * @param {[Entry]} data
+ * @param {String} attribute attribute to use
+ * @param {String} where query path
+ * @param {Number} width
+ * @param {Number} height
+ * @param {Number} margin
+ */
+const drawDonut = (data, attribute, where, width, height, margin) => {
+    const radius = Math.min(width, height) / 2 - margin;
+    const svg = d3
+        .select(where)
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr(
+            "transform",
+            `translate(${width / 2}, ${height / 2})`
+        );
+
+    const entries = data.map((e) => e[attribute]);
+    const differentValues = new Set(entries);
+    const occurrences = entries.reduce(
+        (acc, curr) => (acc[curr] ? ++acc[curr] : (acc[curr] = 1), acc),
+        {}
+    ); // what: how many
+    const counts = Object.keys(occurrences).map((key) => [Number(key), occurrences[key]]);
+
+    // prepare the color scheme
+    const color = d3
+        .scaleOrdinal()
+        .domain(Array.from(differentValues))
+        .range(d3.schemeDark2);
+
+    // Compute the position of each group on the pie:
+    const pie = d3
+        .pie()
+        .sort(null) // Do not sort group by size
+        .value((d) => d[1]);
+    const data_ready = pie(counts);
+
+    // The arc generator
+    const arc = d3
+        .arc()
+        .innerRadius(radius * 0.5) // This is the size of the donut hole
+        .outerRadius(radius * 0.8);
+
+    // Another arc that won't be drawn. Just for labels positioning
+    const outerArc = d3
+        .arc()
+        .innerRadius(radius * 0.9)
+        .outerRadius(radius * 0.9);
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg.selectAll("allSlices")
+        .data(data_ready)
+        .join("path")
+        .attr("d", arc)
+        .attr("fill", (d) => color(d.data[0])) // TODO: corrent?
+        .attr("stroke", "var(--color-font-primary)")
+        .style("stroke-width", "2px")
+        .style("opacity", 0.7)
+        .on("click", (e, segment) => {
+            // TODO: callback
+            // TODO: highlight?
+            console.log({e, segment});
+            e.target.classList.toggle("highlight--donut");
+            console.log(segment.data);
+        });
+
+    // Add the polylines between chart and labels:
+    svg.selectAll("allPolylines")
+        .data(data_ready)
+        .join("polyline")
+        .style("stroke", "var(--color-font-primary)")
+        .style("fill", "none")
+        .attr("stroke-width", 2)
+        .attr("points", function (d) {
+            const posA = arc.centroid(d); // line insertion in the slice
+            const posB = outerArc.centroid(d); // line break: we use the other arc generator that has been built only for that
+            const posC = outerArc.centroid(d); // Label position = almost the same as posB
+            const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2; // we need the angle to see if the X position will be at the extreme right or extreme left
+            posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+            return [posA, posB, posC];
+        });
+
+    // Add the polylines between chart and labels:
+    svg.selectAll("allLabels")
+        .data(data_ready)
+        .join("text")
+        .text((d) => `${d.data[0]} (count: ${d.data[1]})`)
+        .style("fill", "var(--color-font-primary)")
+        .attr("transform", function (d) {
+            const pos = outerArc.centroid(d);
+            const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
+            pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+            return `translate(${pos})`;
+        })
+        .style("text-anchor", function (d) {
+            const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
+            return midangle < Math.PI ? "start" : "end";
+        });
+
+    return svg;
+};
--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,6 @@ var road_pollys;  // TODO: remove
 var map; // TODO: remove
 
 const initialize = async (use_clusters = false) => {
-    return;  // TODO: remove
     // load all the data, create necessary points
     // show loading screen during this
     // initialize global vars
@@ -62,4 +61,4 @@ initialize().then(() => {
     document.documentElement.classList.add("ready");
 })
 
-// TODO: add preloader and use async calls.. https://codepen.io/balgot/pen/gOjgEQm
+drawDonut(donutData, donutAttr, "#sex", 500, 500, 100) // TODO: remove
